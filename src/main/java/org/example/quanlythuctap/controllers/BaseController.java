05625efcff1af0aed03796eeb972b3712c9c3fbd
Chien <chien@gmail.com>
2024-01-23 14:19:08 +0700

    lan1

--- src/main/java/org/example/quanlythuctap/controllers/BaseController.java
+++ src/main/java/org/example/quanlythuctap/controllers/BaseController.java
@@ -3,8 +3,7 @@ package org.example.quanlythuctap.controllers;
 public class BaseController {
     protected boolean xacThuc(String username, String password) {
         String taiKhoan = "abc";
-
-        String matKhau = "12311";
+        String matKhau = "123";
         return taiKhoan.equals(username) && matKhau.equals(password);
     }
 }
